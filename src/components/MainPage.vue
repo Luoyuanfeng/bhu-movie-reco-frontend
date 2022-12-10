34648be831a9bac4f48df6241a9499d444c4d954
luoyuanfeng <luoyuanfeng@meituan.com>
2022-12-10 18:50:49 +0800

    build

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -3,6 +3,8 @@
     <el-menu class="sticky" :default-active="activeIndex" mode="horizontal" :ellipsis="false" router>
       <img class="logo-cls" src="../assets/logo.png" />
       <div class="flex-grow" />
+      <div v-if="isLogin" class="uid">当前登录用户：{{showUid}}</div>
+      <div v-if="!isLogin" class="uid">当前未登录</div>
       <el-menu-item :disabled="!isLogin" index="/sort">推荐列表</el-menu-item>
       <el-menu-item :disabled="!isLogin" index="/recall">召回列表</el-menu-item>
       <el-menu-item :disabled="!isLogin" index="/history">观看历史</el-menu-item>
@@ -21,10 +23,12 @@ import LoginPage from './LoginPage.vue'
 const router = useRouter()
 const activeIndex = ref('')
 const isLogin = ref(false)
+const showUid = ref('')
 
 const handelLogin = function(uid) {
   console.log('======>' + uid);
   sessionStorage.setItem('bmr-uid', uid)
+  showUid.value = uid
   isLogin.value = true
   activeIndex.value = '/history'
   router.push("/history")
@@ -34,6 +38,12 @@ const handelLogin = function(uid) {
 </script>
 
 <style>
+.uid {
+  height: 56px;
+  line-height: 56px;
+  color: #333;
+  margin-right: 20px;
+}
 .flex-grow {
   flex-grow: 1;
 }
